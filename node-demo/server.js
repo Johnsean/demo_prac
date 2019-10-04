fa4f31a258475994db0082b3ec66539f65393d1f
John <956407921@qq.com>
2019-10-04 20:06:22 +0800

    不用数据库扣款

--- node-demo/server.js
+++ node-demo/server.js
@@ -20,7 +20,7 @@ var server = http.createServer(function (request, response) {
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('HTTP 路径为\n' + path)
-  if (path == '/style.js') {
+  if (path == '/style.js') { // 第一种写法 直接写入输出
     response.setHeader('Content-Type', 'text/css; charset=utf-8')
     response.write('body{background-color: #ddd;}h1{color: red;}')
     response.end()
@@ -37,9 +37,14 @@ var server = http.createServer(function (request, response) {
       '<script src="/script.html"></script>' +
       '</body></html>')
     response.end()
+  }else if (path === '/') { // 第二种写法 读取文件写入输出
+    var string = fs.readFileSync('./index.html', 'utf8')
+    response.setHeader('Content-Type', 'text/html; charset=utf-8')
+    response.end(string)
   }else {
     response.statusCode = 404
-    response.end()
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.end('找不到对应的路径，需要自行修改index')
   }
 
 /******** 代码结束，下面不要看 ************/
